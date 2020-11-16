8a1fda3456c609a67788568ec7076c6d2097d437
romel diaz <root.romel@gmail.com>
2020-11-15 19:07:55 -0500

    domingo 3

--- database/migrations/2020_11_13_225359_create_progs_view.php
+++ database/migrations/2020_11_13_225359_create_progs_view.php
@@ -13,25 +13,25 @@ class CreateProgsView extends Migration
      */
     public function up()
     {
-        DB::statement("create or replace view view_progs as
-        select
+        DB::statement("CREATE OR REPLACE view view_progs AS
+        SELECT
         pro.date, pro.company_id,
-        prof.id professional_id, prof.fullname as professional_fullname, 
-        prof.profession as professional_profession, prof.code as professional_code,
-        rety.id as regimen_type_id, rety.descrip as regimen_type_descrip,
-        foty.id as food_type_id, foty.descrip as food_type_descrip,
-        pre.id as preparation_id, pre.descrip as preparation_descrip,
+        prof.id professional_id, prof.fullname AS professional_fullname, 
+        prof.profession AS professional_profession, prof.code AS professional_code,
+        rety.id AS regimen_type_id, rety.descrip AS regimen_type_descrip,
+        foty.id AS food_type_id, foty.descrip AS food_type_descrip,
+        pre.id AS preparation_id, pre.descrip AS preparation_descrip,
         pred.factor_unit_id,
-        foo.id as food_id, foo.descrip as food_descrip,
+        foo.id AS food_id, foo.descrip AS food_descrip,
         pred.amount
-        from preparation_details as pred
-        left join programming_details as prod on prod.preparation_id = pred.preparation_id
-        left join programmings as pro on pro.id = prod.programming_id
-        left join preparations as pre on pre.id = pred.preparation_id
-        left join foods as foo on foo.id = pred.food_id
-        left join food_types as foty on foty.id = pro.food_type_id
-        left join regimen_types as rety on rety.id = pro.regimen_type_id
-        left join professionals as prof on prof.id = pro.professional_id
+        FROM preparation_details AS pred
+        LEFT JOIN programming_details AS prod ON prod.preparation_id = pred.preparation_id
+        LEFT JOIN programmings AS pro ON pro.id = prod.programming_id
+        LEFT JOIN preparations AS pre ON pre.id = pred.preparation_id
+        LEFT JOIN foods AS foo ON foo.id = pred.food_id
+        LEFT JOIN food_types AS foty ON foty.id = pro.food_type_id
+        LEFT JOIN regimen_types AS rety ON rety.id = pro.regimen_type_id
+        LEFT JOIN professionals AS prof ON prof.id = pro.professional_id
         ");
 
         
@@ -44,6 +44,55 @@ class CreateProgsView extends Migration
                 set result = if(nut_grams=0 or nut_grams is null, 0, (nut_value*food_amount/nut_grams) );
                 RETURN result;
         END");
+
+        DB::statement("CREATE OR REPLACE view view_nutrients AS
+        SELECT
+        vpro.*,
+        (SELECT three_rule(nut.grams, nut.energy_kcal, vpro.amount)) AS energy_kcal,
+        (SELECT three_rule(nut.grams, nut.water_g, vpro.amount)) AS water_g,
+        (SELECT three_rule(nut.grams, nut.proteinins_g, vpro.amount)) AS proteinins_g,
+        (SELECT three_rule(nut.grams, nut.total_fat_g, vpro.amount)) AS total_fat_g,
+        (SELECT three_rule(nut.grams, nut.total_carbohydrates_g, vpro.amount)) AS total_carbohydrates_g,
+        (SELECT three_rule(nut.grams, nut.disp_carbohydrates_g, vpro.amount)) AS disp_carbohydrates_g,
+        (SELECT three_rule(nut.grams, nut.raw_fiber_g, vpro.amount)) AS raw_fiber_g,
+        (SELECT three_rule(nut.grams, nut.dietary_fiber_g, vpro.amount)) AS dietary_fiber_g,
+        (SELECT three_rule(nut.grams, nut.calcium_mg, vpro.amount)) AS calcium_mg,
+        (SELECT three_rule(nut.grams, nut.match_mg, vpro.amount)) AS match_mg,
+        (SELECT three_rule(nut.grams, nut.zinc_mg, vpro.amount)) AS zinc_mg,
+        (SELECT three_rule(nut.grams, nut.iron_mg, vpro.amount)) AS iron_mg,
+        (SELECT three_rule(nut.grams, nut.carotene_ug, vpro.amount)) AS carotene_ug,
+        (SELECT three_rule(nut.grams, nut.retinol_ug, vpro.amount)) AS retinol_ug,
+        (SELECT three_rule(nut.grams, nut.vitamin_a_ug, vpro.amount)) AS vitamin_a_ug,
+        (SELECT three_rule(nut.grams, nut.thiamine_mg, vpro.amount)) AS thiamine_mg,
+        (SELECT three_rule(nut.grams, nut.reboflavin_mg, vpro.amount)) AS reboflavin_mg,
+        (SELECT three_rule(nut.grams, nut.niacin_mg, vpro.amount)) AS niacin_mg,
+        (SELECT three_rule(nut.grams, nut.vitamin_c_mg, vpro.amount)) AS vitamin_c_mg,
+        (SELECT three_rule(nut.grams, nut.asct_g, vpro.amount)) AS asct_g,
+        (SELECT three_rule(nut.grams, nut.sodium_mg, vpro.amount)) AS sodium_mg,
+        (SELECT three_rule(nut.grams, nut.cholesterol_mg, vpro.amount)) AS cholesterol_mg,
+        (SELECT three_rule(nut.grams, nut.potassium_mg, vpro.amount)) AS potassium_mg,
+        (SELECT three_rule(nut.grams, nut.omega3, vpro.amount)) AS omega3,
+        (SELECT three_rule(nut.grams, nut.omega6, vpro.amount)) AS omega6,
+        (SELECT three_rule(nut.grams, nut.saturated_fat_g, vpro.amount)) AS saturated_fat_g,
+        (SELECT three_rule(nut.grams, nut.trans_fat_g, vpro.amount)) AS trans_fat_g,
+        (SELECT three_rule(nut.grams, nut.magnetium_mg, vpro.amount)) AS magnetium_mg,
+        (SELECT three_rule(nut.grams, nut.iodine_mg, vpro.amount)) AS iodine_mg,
+        (SELECT three_rule(nut.grams, nut.selenium_ug, vpro.amount)) AS selenium_ug,
+        (SELECT three_rule(nut.grams, nut.folic_acid_ug, vpro.amount)) AS folic_acid_ug,
+        (SELECT three_rule(nut.grams, nut.vitamin_d_ug, vpro.amount)) AS vitamin_d_ug,
+        (SELECT three_rule(nut.grams, nut.vitamin_b1_mg, vpro.amount)) AS vitamin_b1_mg,
+        (SELECT three_rule(nut.grams, nut.vitamin_b2_mg, vpro.amount)) AS vitamin_b2_mg,
+        (SELECT three_rule(nut.grams, nut.b12_vitamin_mg, vpro.amount)) AS b12_vitamin_mg,
+        (SELECT three_rule(nut.grams, nut.copper_ug, vpro.amount)) AS copper_ug,
+        (SELECT three_rule(nut.grams, nut.mercury_ug, vpro.amount)) AS mercury_ug,
+        (SELECT three_rule(nut.grams, nut.lead_ug, vpro.amount)) AS lead_ug,
+        (SELECT three_rule(nut.grams, nut.aluminum_mg, vpro.amount)) AS aluminum_mg,
+        (SELECT three_rule(nut.grams, nut.cadmium_ug, vpro.amount)) AS cadmium_ug,
+        (SELECT three_rule(nut.grams, nut.arsenic_ug, vpro.amount)) AS arsenic_ug,
+        (SELECT three_rule(nut.grams, nut.dha_mg, vpro.amount)) AS dha_mg,
+        (SELECT three_rule(nut.grams, nut.ara_mg, vpro.amount)) AS ara_mg
+        FROM view_progs AS vpro
+        LEFT JOIN nutrients AS nut ON nut.food_id = vpro.food_id");
     }
 
     /**
@@ -53,6 +102,6 @@ class CreateProgsView extends Migration
      */
     public function down()
     {
-        DB::statement('drop view if exists view_progs');
+        //DB::statement('drop view if exists view_progs');
     }
 }
